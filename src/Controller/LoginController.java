124aeed2dd7191d955bd21cf413a44a547144249
Johanna <johanna.dahlborn@gmail.com>
2021-02-04 12:17:36 +0100

    Add
    add

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,8 +1,25 @@
 package Controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.fxml.FXML;
+
 public class LoginController {
 
-    public void LoginAdmin(){
+
+    public PasswordField passwordTxF;
+    public Label forgotPasswordBTN;
+    public TextField usernameTxtF;
+    public Button signinBTN;
+
+    public void LoginAdmin(MouseEvent mouseEvent) {
+
+}
+
+    public void forgotPsw(MouseEvent mouseEvent) {
 
     }
 }
