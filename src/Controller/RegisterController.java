a8eaa3d2e1ee96a4ee7a503894b13534e3ef8c03
Johanna <johanna.dahlborn@gmail.com>
2021-02-04 19:20:55 +0100

    add
    add

---
+++ src/Controller/RegisterController.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class RegisterController {
+    @FXML
+    public Button registerButton;
+    @FXML
+    public TextField firstnameTextfield;
+    @FXML
+    public TextField mailTextfield;
+    @FXML
+    public TextField numberTextfield;
+    @FXML
+    public TextField addressTextfield;
+    @FXML
+    public TextField lastnameTextfield;
+    @FXML
+    public TextField cityTextfield;
+    @FXML
+    public ComboBox countryCombobox;
+    @FXML
+    public TextField passwordTextfield;
+
+
+    public void registerActionEvent(ActionEvent actionEvent) {
+        
+
+    }
+}
