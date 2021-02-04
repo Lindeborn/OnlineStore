ad9cfb6dcf6d0001f48731833542e563de7bfa13
lannamaslo <lanna.maslo@icloud.com>
2021-02-04 12:03:23 +0100

    connection

--- src/Controller/SqlServerConnection.java
+++ src/Controller/SqlServerConnection.java
@@ -1,4 +1,35 @@
 package Controller;
 
+import java.sql.*;
+
 public class SqlServerConnection {
+    public SqlServerConnection(){
+
+    }
+
+    public Connection connect() throws SQLException, ClassNotFoundException {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String dbURL = "jdbc:sqlserver://localhost";
+
+        String user = "Lanna";
+        String password = "password123";
+
+        Connection connection = DriverManager.getConnection(dbURL, user, password);
+        if (connection != null) {
+            System.out.println("connected");
+            return connection;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args){
+        SqlServerConnection sql = new SqlServerConnection();
+        try{
+            Connection connection = sql.connect();
+
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
