49d4d3bf5de8cfe58ba653002b0b74d7bba8eb4a
lannamaslo <lanna.maslo@icloud.com>
2021-02-09 22:54:10 +0100

    :)))

--- src/customer/LoginCustomerController.java
+++ src/customer/LoginCustomerController.java
@@ -18,7 +18,15 @@ public class LoginCustomerController {
     public TextField usernameTxtF;
     public Button signinBTN;
     public Button newCustomerButton;
+    public SqlServerConnection ConnectNow;
+    public Connection connectionDB;
+    public RegisterController reg = new RegisterController();
 
+
+    public LoginCustomerController() throws SQLException, ClassNotFoundException {
+        ConnectNow = new SqlServerConnection();
+        connectionDB = ConnectNow.connect();
+    }
 
     public void loginButtonAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
         if (!usernameTxtF.getText().isBlank() && !passwordTxF.getText().isBlank()) {
@@ -30,13 +38,8 @@ public class LoginCustomerController {
     }
 
     public void validateLogin() throws SQLException, ClassNotFoundException {
-        SqlServerConnection ConnectNow = new SqlServerConnection();
-        Connection connectionDB = ConnectNow.connect();
-
-        Statement statement = connectionDB.createStatement();
-
         try {
-            PreparedStatement ps = connectionDB.prepareStatement("SELECT Email, Password FROM Email WHERE Email = ? AND Password = ?");
+            PreparedStatement ps = connectionDB.prepareStatement("SELECT Email, Password FROM Customer WHERE Email = ? AND Password = ?");
             ps.setString(1, usernameTxtF.getText());
             ps.setString(2, passwordTxF.getText());
             ResultSet result = ps.executeQuery();
@@ -48,34 +51,29 @@ public class LoginCustomerController {
             }
         } catch (SQLException ex) {
         }
-
-        statement.executeUpdate("INSERT INTO Email " + "VALUES ('" + usernameTxtF.getText() + "', " + "'" + passwordTxF.getText() + "')");
-        connectionDB.close();
-
-        /*String verifyLogin  = "Select " + usernameTxtF.getText() ;
-
-        try{
-            Statement statement = connectionDB.createStatement();
-            ResultSet queryResult = statement.executeQuery(verifyLogin);
-
-            while(queryResult.next()){
-                if(queryResult.getInt(1) ==1){
-                    //loginMessageLabel.setText("Congratulations!");
-                }else{
-                    //loginMessageLabel.setText("Invalid login");
-                }
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }*/
-
     }
-    public void createAccountForm(ActionEvent event) throws IOException {
+
+    public void createAccountForm(ActionEvent event) throws IOException, SQLException {
         Stage stage = (Stage) newCustomerButton.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("RegisterNewCustomer.fxml"));
 
         Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void registerCustomer() throws SQLException {
+        Statement statement = connectionDB.createStatement();
+        statement.executeUpdate("INSERT INTO Customer "
+                + "VALUES ('" + reg.mailTextfield.getText()
+                + "', '" + reg.numberTextfield.getText()
+                + "', '" + reg.firstnameTextfield.getText()
+                + "', '" + reg.lastnameTextfield.getText()
+                + "', '" + reg.addressTextfield.getText()
+                + "', '" + reg.countryCombobox.getValue().toString()
+                + "', '" + reg.cityTextfield.getText()
+                + "', '" + reg.setPasswordTextfield.getText()
+                + "')");
+        connectionDB.close();
     }
 }
