7b97ec9b750a63bdff5ddca8d5e68e9609c2829d
Johanna <johanna.dahlborn@gmail.com>
2021-02-16 17:34:18 +0100

    Add
    add

--- src/customer/ShopController.java
+++ src/customer/ShopController.java
@@ -1,4 +1,22 @@
 package customer;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
 public class ShopController {
+    public Button addButton;
+    public Button removeButton;
+    public ListView basketList;
+    public ListView itemsList;
+    public Button editButton;
+
+    public void addAction(ActionEvent actionEvent) {
+    }
+
+    public void removeAction(ActionEvent actionEvent) {
+    }
+
+    public void editAction(ActionEvent actionEvent) {
+    }
 }
