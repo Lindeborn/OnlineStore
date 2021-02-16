35c3ca177aff8d263ff47beea342ed5c0dede819
Johanna <johanna.dahlborn@gmail.com>
2021-02-16 16:05:23 +0100

    add
    add

--- src/customer/RegisterController.java
+++ src/customer/RegisterController.java
@@ -3,8 +3,13 @@ package customer;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 
@@ -37,22 +42,28 @@ public class RegisterController {
 
 
 
-    public void registerActionEvent(ActionEvent actionEvent) throws SQLException {
-        newUserLBL.setText("User has been registered successfully");
+    public void registerActionEvent(ActionEvent actionEvent) throws SQLException, IOException {
         lgCustomer.registerCustomer();
         registerUser();
 
     }
 
-    private void registerUser() {
+    private void registerUser() throws IOException {
         if (setPasswordTextfield.getText().equals(passwordTextfield1.getText())){
-            confirmPWLBL.setText("You are set");
+            System.out.println("you are set");
+            Stage stage = (Stage) registerButton.getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource("ShopCustomer.fxml"));
+
+            Scene scene = new Scene(root, 600, 400);
+            stage.setScene(scene);
+            stage.show();
+
         }  else {
             confirmPWLBL.setText("Password does not match");
         }
     }
 
-    
+
     public void clearAction(ActionEvent actionEvent) {
         firstnameTextfield.setText("");
         lastnameTextfield.setText("");
